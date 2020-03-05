df628e1013c7894444f29af690c8e01eddf3a231
Cyber Kommando <cyberubenex@hotmail.com>
2020-03-05 09:33:31 +0000

    LP1alua02ex1

---
+++ IncDec/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+
+            Console.WriteLine(a++ + b++);
+
+            int c = 7;
+            int d = 9;
+
+            Console.WriteLine(--c + --d);
+        }
+    }
+}
