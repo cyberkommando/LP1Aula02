9a7fd16a8221642d9add9784152ceec959983303
Cyber Kommando <cyberubenex@hotmail.com>
2020-03-05 10:42:16 +0000

    LP1Aula02ex4

---
+++ Cilindro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarar variáveis
+            string aString, rString;
+            float a, r, v, surfArea;
+            const float pi = 3.1415926f;
+            // 1. pedir ao utilizador os dados
+            Console.Write("Coloque a altura:");
+            aString = Console.ReadLine();
+            Console.Write("Coloque o raio:");
+            rString = Console.ReadLine();
+            // 2. converter input para float
+            a = float.Parse(aString);
+            r = Convert.ToSingle(rString);
+            // 3. Fazer as contas 
+            v = pi * r * r * a;
+            surfArea = 2 * pi * r * (r + a);
+            // 4. Apresentar resultados
+            Console.WriteLine($"Volume    = {v}");
+            Console.WriteLine($"surfArea    = {surfArea}");
+        }
+    }
+}
