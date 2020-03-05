73a48ff71b29c216e969cd406538dd8fb172e45e
Cyber Kommando <cyberubenex@hotmail.com>
2020-03-05 11:41:28 +0000

    ValoreEspeciais_para_compreender_valores_especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(float.PositiveInfinity);
+            Console.WriteLine(float.NegativeInfinity);
+            Console.WriteLine(double.PositiveInfinity);
+            Console.WriteLine(double.NegativeInfinity);
+        }
+    }
+}
